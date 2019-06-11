8505f14a380826d3363fa27703cbf5e22d62287b
aurnoi1 <11244735+aurnoi1@users.noreply.github.com>
2019-06-11 09:15:20 -0400

    - Move ThinkTime and AdjustTimeout from IMySession to ISession.

--- tests/IC.Tests.App.UIAccessibility/Appium/Interfaces/IMySession.cs
+++ tests/IC.Tests.App.UIAccessibility/Appium/Interfaces/IMySession.cs
@@ -7,17 +7,5 @@ namespace IC.Tests.App.UIAccessibility.Appium.Interfaces
 {
     public interface IMySession : IMyNavigables, IWindowsDriverSession
     {
-
-        /// <summary>
-        /// Multiplicator to adjust the timeout to the environment when waiting for the controls.
-        /// </summary>
-        uint ThinkTime { get; set; }
-
-        /// <summary>
-        /// Adjust the timeout to the environment when waiting for the controls depending the <see cref="ThinkTime"/> value.
-        /// </summary>
-        /// <param name="timeout">The timeout.</param>
-        /// <returns>The adjusted timeout.</returns>
-        TimeSpan AdjustTimeout(TimeSpan timeout);
     }
 }
