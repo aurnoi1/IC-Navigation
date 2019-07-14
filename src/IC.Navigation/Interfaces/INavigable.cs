549614440da821bebaad5327e41af6e0aef3c0d2
aurnoi1 <11244735+aurnoi1@users.noreply.github.com>
2019-07-14 11:40:14 -0400

    Move implementation of INavigableObservable to INavigable.

--- src/IC.Navigation/Interfaces/INavigable.cs
+++ src/IC.Navigation/Interfaces/INavigable.cs
@@ -6,7 +6,7 @@ namespace IC.Navigation.Interfaces
     /// <summary>
     /// Defines an INavigable.
     /// </summary>
-    public interface INavigable : INavigableObservable
+    public interface INavigable
     {
         /// <summary>
         /// The navigation session.
@@ -23,5 +23,25 @@ namespace IC.Navigation.Interfaces
         /// </summary>
         /// <returns>A Dictionary of action to go to the next INavigable.</returns>
         Dictionary<INavigable, Action> GetActionToNext();
+
+        /// <summary>
+        /// Register the INavigableObserver as a WeakReference.
+        /// </summary>
+        /// <param name="observer">The INavigableObserver.</param>
+        /// <returns>The INavigableObserver as a WeakReference.</returns>
+        WeakReference<INavigableObserver> RegisterObserver(INavigableObserver observer);
+
+        /// <summary>
+        /// Unregister the INavigableObserver.
+        /// </summary>
+        /// <param name="weakObserver">The INavigableObserver as a WeakReference.</param>
+        void UnregisterObserver(WeakReference<INavigableObserver> weakObserver);
+
+        /// <summary>
+        /// Notify all observers.
+        /// </summary>
+        /// <param name="args">The INavigableEventArgs.</param>
+        void NotifyObservers(INavigableEventArgs args);
+
     }
 }
