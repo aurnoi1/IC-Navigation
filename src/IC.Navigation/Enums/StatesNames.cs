8b2081b6a059a1bd9ff9d617518d217f81fe0c94
aurnoi1 <11244735+aurnoi1@users.noreply.github.com>
2019-11-21 12:03:01 -0500

    Add State as futur object of NavigableStatus

---
+++ src/IC.Navigation/Enums/StatesNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IC.Navigation.Enums
+{
+    public enum StatesNames
+    {
+        Exist = 0,
+        Ready = 1
+    }
+}
